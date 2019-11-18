b419f78976c20a991e353a3a850d1d4c1ee563a7
Blanchard, Lucas <lucas.blanchard@accenture.com>
2019-11-18 11:42:40 -0600

    adjust upload script

--- upload-lambda.py
+++ upload-lambda.py
@@ -1,14 +1,19 @@
 import zipfile
 import boto3
 import mimetypes
+import os
+import tempfile
 
-s3 = boto3.client('s3')
-s3.download_file('portfolio-build-blanchlu',
-                 'buildPortfolio', './buildPortfolio.zip')
-
-with zipfile.ZipFile('./buildPortfolio.zip') as myzip:
-    for nm in myzip.namelist():
-        s3.upload_file(nm, 'serverless-practice-blanchlu', nm,
-                       ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        s3.put_object_acl(Bucket='serverless-practice-blanchlu',
-                          Key=nm, ACL='public-read')
+def lambda_handler(event, context):
+    s3 = boto3.client('s3')
+    with tempfile.TemporaryDirectory() as tmpdir:
+        s3.download_file('portfolio-build-blanchlu',
+                    'buildPortfolio', os.path.join(tmpdir, 'buildPortfolio.zip'))
+        os.chdir(tmpdir)
+        with zipfile.ZipFile('buildPortfolio.zip', 'r') as myzip:
+            myzip.extractall()
+            for nm in myzip.namelist():
+                s3.upload_file(nm, 'serverless-practice-blanchlu', nm,
+                              ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                s3.put_object_acl(Bucket='serverless-practice-blanchlu',
+                                  Key=nm, ACL='public-read')
