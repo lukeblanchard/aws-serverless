4ab62a6b3720b0923b3630d1a804f0229f519493
Blanchard, Lucas <lucas.blanchard@accenture.com>
2019-11-18 12:58:10 -0600

    changes to update

--- upload-lambda.py
+++ upload-lambda.py
@@ -6,6 +6,7 @@ import tempfile
 
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
+    sns = boto3.resource('sns')
     with tempfile.TemporaryDirectory() as tmpdir:
         s3.download_file('portfolio-build-blanchlu',
                     'buildPortfolio', os.path.join(tmpdir, 'buildPortfolio.zip'))
@@ -17,3 +18,5 @@ def lambda_handler(event, context):
                               ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
                 s3.put_object_acl(Bucket='serverless-practice-blanchlu',
                                   Key=nm, ACL='public-read')
+    topic = sns.Topic('arn:aws:sns:us-east-1:171754680472:deployPortfolioTopic')
+    topic.publish(Subject='Build Deployed', Message='A build has been deployed')
